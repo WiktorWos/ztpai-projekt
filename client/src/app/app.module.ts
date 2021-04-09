4a8c7c68a9218763fac41cdbeed8468256c7762e
WiktorWos <wiktor.wos99@gmail.com>
2021-04-09 23:06:10 +0200

    set up meeting view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HourPickerComponent } from './hour-picker/hour-picker.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SetUpMeetingComponent } from './set-up-meeting/set-up-meeting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    HourPickerComponent,
+    NavbarComponent,
+    SetUpMeetingComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/set-up-meeting/set-up-meeting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-up-meeting',
+  templateUrl: './set-up-meeting.component.html',
+  styleUrls: ['./set-up-meeting.component.scss']
+})
+export class SetUpMeetingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
