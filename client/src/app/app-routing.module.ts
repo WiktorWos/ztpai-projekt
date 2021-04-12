fd040f917f8e0f619a15e2e98deb70c511316d8e
WiktorWos <wiktor.wos99@gmail.com>
2021-04-12 21:32:28 +0200

    basic routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {SetUpMeetingComponent} from './set-up-meeting/set-up-meeting.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'setUp', component: SetUpMeetingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
