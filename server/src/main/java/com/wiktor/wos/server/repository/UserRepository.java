fb39842e5bac9f6a59678d25cfdbd3acd49c98eb
WiktorWos <wiktor.wos99@gmail.com>
2021-05-17 17:12:45 +0200

    creating JwtTokenProvider

--- server/src/main/java/com/wiktor/wos/server/repository/UserRepository.java
+++ server/src/main/java/com/wiktor/wos/server/repository/UserRepository.java
@@ -4,4 +4,8 @@ import com.wiktor.wos.server.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String login);
+
+    User findByToken(String token);
 }
