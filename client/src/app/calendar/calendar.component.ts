98153fbbec9cbfe3bf117507ff69fd32be236bcb
WiktorWos <wiktor.wos99@gmail.com>
2021-06-08 14:05:12 +0200

    working calendar component

--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+  date: Date;
+  days: number[];
+  prevDays: number[];
+  monthList = [
+    { value: 1, text: 'Jan' },
+    { value: 2, text: 'Feb'},
+    { value: 3, text: 'Mar' },
+    { value: 4, text: 'Apr' },
+    { value: 5, text: 'May' },
+    { value: 6, text: 'June' },
+    { value: 7, text: 'July' },
+    { value: 8, text: 'Aug' },
+    { value: 9, text: 'Sep' },
+    { value: 10, text: 'Oct' },
+    { value: 11, text: 'Nov' },
+    { value: 12, text: 'Dec' }
+  ];
 
-  constructor() { }
+  selectedMonth: number;
 
-  ngOnInit(): void {
+  constructor() {
+    this.days = [];
+    this.prevDays = [];
+    this.date = new Date();
+    this.selectedMonth = this.date.getMonth() + 1;
   }
 
+  ngOnInit(): void {
+    this.days = [];
+    this.prevDays = [];
+    const year = this.date.getFullYear();
+    this.renderDays(this.selectedMonth, year);
+  }
+
+  renderDays(month, year): void {
+    let firstDayOfWeek = this.getFirstOfMonthDayOfWeek(month - 1, year);
+    firstDayOfWeek = firstDayOfWeek === 0 ? 7 : firstDayOfWeek;
+    const daysNum = this.getDaysInMonth(month, year);
+    const previousDaysNum = this.getDaysInMonth(month - 1, year);
+    for (let i = previousDaysNum - firstDayOfWeek + 1; i < previousDaysNum; i++) {
+      this.prevDays.push(i + 1);
+    }
+    for (let i = 1; i <= daysNum; i++) {
+      this.days.push(i);
+    }
+  }
+
+  getDaysInMonth(month, year): number {
+    return new Date(year, month, 0).getDate();
+  }
+
+  getFirstOfMonthDayOfWeek(month, year): number {
+    const firstDayOfMonth = new Date(year, month, 1);
+    return firstDayOfMonth.getDay();
+  }
+
+  onChangeObj(newObj) {
+    this.selectedMonth = newObj;
+    this.ngOnInit();
+  }
 }
